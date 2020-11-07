fdee7c360938489ac18f7ff03b15953f335819ed
Juan Carlos Cuasquen <juan.cuasquen@uao.edu.co>
2020-11-07 18:23:50 -0500

    agregando react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import Login from './Components/Login'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <div  style={{margin:"2em 0"}}>
+          <Switch>
+            <Route exact path="/" component={Login}/>
+          </Switch>  
+        </div>
+        
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Components/Login.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+class Login extends React.Component {
+
+    constructor (props, context){
+        super(props, context);
+
+        // Prepare simple state
+        this.state = {
+            
+        };
+
+     
+    }
+  
+    
+
+    render(){
+        return (
+            <div>Hola</div>
+        );
+    }
+};
+
+export default Login;
